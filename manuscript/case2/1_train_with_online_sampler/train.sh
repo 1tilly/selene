241563b93baff40b5403f4aa78dae4782633aef6
Tobias Tilly <tobias@tilly.io>
2021-06-07 17:39:08 +0100

    Forked Selene

--- manuscript/case2/1_train_with_online_sampler/train.sh
+++ manuscript/case2/1_train_with_online_sampler/train.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-#SBATCH --time=7-00:00:00
-#SBATCH --gres=gpu:4
-#SBATCH --partition=gpu
-#SBATCH --constraint=v100
-#SBATCH -o train_online_%j.out
-#SBATCH -e train_online_%j.err
-
-source activate selene-env
-
-python -u ../../../selene_cli.py ./train_online_sampler.yml --lr=0.08
+/home/tt419/anaconda2/envs/Selene/bin/python -u /home/tt419/Projects/DeepLearning/selene/selene_sdk/cli.py ./train_online_sampler.yml --lr=0.08
